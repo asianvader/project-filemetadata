d41f72a3a854a79b740c597c2fe55eadb3c652e8
asianvader <phoebe.voong@gmail.com>
2019-09-09 17:04:18 +0100

    add multer and create post function to upload file

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-// require and use "multer"...
+const app = express();
 
-var app = express();
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+}); 
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,6 +16,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post("/", upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if(!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
